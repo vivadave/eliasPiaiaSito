be83e68db617b52ef201b6e5fa92e72b7eacc9fc
vivadave <vivadave@hotmail.com>
2024-05-10 08:53:14 +0200

    fix consfig

--- svelte.config.js
+++ svelte.config.js
@@ -1,32 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// default options are shown
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			relative: false,
-		},
-		alias: {
-      $lib: "src/lib",
-    },
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false
+		})
 	}
 };
-
-export default config;
